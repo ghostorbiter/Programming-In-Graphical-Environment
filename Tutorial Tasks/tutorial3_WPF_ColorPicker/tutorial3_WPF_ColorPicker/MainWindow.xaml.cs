27ebe551770ce8d5f8066567954d317dd3384d39
ghostorbiter <109592924+ghostorbiter@users.noreply.github.com>
2022-10-24 14:56:27 +0200

    Add files via upload

---
+++ Tutorial Tasks/tutorial3_WPF_ColorPicker/tutorial3_WPF_ColorPicker/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tutorial3_WPF_ColorPicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    class SliderToColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var red = System.Convert.ToByte(values[0]);
+            var green = System.Convert.ToByte(values[1]);
+            var blue = System.Convert.ToByte(values[2]);
+
+            return new SolidColorBrush(Color.FromArgb(255, red, green, blue));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
---
+++ Tutorial Tasks/tutorial3_WPF_ColorPreview/tutorial3_WPF_ColorPreview/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tutorial3_WPF_ColorPreview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    class ColorToBrushConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Color))
+                return null;
+
+            return new SolidColorBrush((Color)value);
+        }
+    }
+    class ColorInfo
+    {
+        public string Name { get; set; }
+        public string RGBinfo { get; set; }
+        public Color RGB { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var props = typeof(Colors).GetProperties(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+            var colorInfos = props.Select(prop =>
+            {
+                var color = (Color)prop.GetValue(null, null);
+                return new ColorInfo()
+                {
+                    Name = prop.Name,
+                    RGB = color,
+                    RGBinfo = $"R:{color.R} G:{color.G} B:{color.B}"
+                };
+            });
+            this.DataContext = colorInfos;
+        }
+    }
+}
